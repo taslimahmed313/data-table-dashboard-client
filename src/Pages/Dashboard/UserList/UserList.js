942e703ac232e44a7b17272fbd688a14feeabe0b
Taslim <taslimahmed313@gmil.com>
2022-12-24 19:26:51 +0600

    Hide Show Cl0umn

--- src/Pages/Dashboard/UserList/UserList.js
+++ src/Pages/Dashboard/UserList/UserList.js
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useState } from 'react';
 import { BiDotsVertical } from 'react-icons/bi';
 import { CiEdit } from 'react-icons/ci';
 import { FaTrash } from 'react-icons/fa';
@@ -12,6 +12,17 @@ import computer from "../../../assets/images/Vector.png";
 import "./UserList.css";
 
 const UserList = () => {
+
+  const [columnShowHide, setColumnShowHide] = useState(false);
+  const [columnShow, setColumnShow] = useState({
+    sl: "sl",
+    user: "user",
+    email: "email",
+    plan: "plan",
+    role: "role",
+    status: "status",
+    action: "action",
+  });
 
     const { data: users = [], refetch } = useQuery({
       queryKey: ["users"],
@@ -26,101 +37,236 @@ const UserList = () => {
       },
     });
 
+    const handleColumnShowHide = () => {
+      setColumnShowHide(!columnShowHide);
+    };
+
+    const handleColumn = (data) => {
+      setColumnShow({
+        ...columnShow,
+        [data.target.name]: data.target.value,
+      });
+    };
+   
+
     return (
       <div className="userList">
         <h1>User List</h1>
+
         <div>
+          <label
+            tabIndex={0}
+            className=""
+            onClick={handleColumnShowHide}
+          >
+            Show/Hide Column
+          </label>
           <div>
-            <table>
+            <label
+              htmlFor="user"
+              className="flex items-center pl-5 border-b py-1"
+            >
+              <input
+                className=""
+                onClick={handleColumn}
+                type="checkbox"
+                name="user"
+                value={columnShow.user === "user" ? "" : "user"}
+                defaultChecked={columnShow.user === "user" ? true : false}
+              />
+              User
+            </label>
+            <label
+              htmlFor="email"
+              className="flex items-center pl-5 border-b py-1"
+            >
+              <input
+                className=""
+                onClick={handleColumn}
+                type="checkbox"
+                name="email"
+                value={columnShow.email === "email" ? "" : "email"}
+                defaultChecked={
+                  columnShow.email === "email" ? true : false
+                }
+              />
+              Email
+            </label>
+            <label
+              htmlFor="plan"
+              className="flex items-center pl-5 border-b py-1"
+            >
+              <input
+                className=""
+                onChange={handleColumn}
+                type="checkbox"
+                name="plan"
+                value={columnShow.plan === "plan" ? "" : "plan"}
+                defaultChecked={columnShow.plan === "plan" ? true : false}
+              />
+              Plan
+            </label>
+            <label
+              htmlFor="role"
+              className="flex items-center pl-5 border-b py-1"
+            >
+              <input
+                className=""
+                onChange={handleColumn}
+                type="checkbox"
+                name="role"
+                value={columnShow.role === "role" ? "" : "role"}
+                defaultChecked={columnShow.role === "role" ? true : false}
+              />
+              Role
+            </label>
+            <label
+              htmlFor="status"
+              className="flex items-center pl-5 border-b py-1"
+            >
+              <input
+                className=""
+                onChange={handleColumn}
+                type="checkbox"
+                name="status"
+                value={columnShow.status === "status" ? "" : "status"}
+                defaultChecked={
+                  columnShow.status === "status" ? true : false
+                }
+              />
+              Status
+            </label>
+            <label htmlFor="action" className="flex items-center pl-5  py-1">
+              <input
+                className=""
+                onChange={handleColumn}
+                type="checkbox"
+                name="action"
+                value={columnShow.action === "action" ? "" : "action"}
+                defaultChecked={
+                  columnShow.action === "action" ? true : false
+                }
+              />
+              Action
+            </label>
+          </div>
+        </div>
+
+        <div>
+          <div id="table">
+            <table className="">
               <thead>
                 <tr>
-                  <th scope="col">USER</th>
-                  <th scope="col">EMAIL</th>
-                  <th scope="col">ROLE</th>
-                  <th scope="col">PLAN</th>
-                  <th scope="col">STATUS</th>
-                  <th scope="col">ACTION</th>
+                  {columnShow.user === "user" && <th scope="col">USER</th>}
+                  {columnShow.email === "email" && (
+                    <th scope="col">EMAIL</th>
+                  )}
+                  {columnShow.role === "role" && <th scope="col">ROLE</th>}
+                  {columnShow.plan === "plan" && <th scope="col">PLAN</th>}
+                  {columnShow.status === "status" && (
+                    <th scope="col">STATUS</th>
+                  )}
+                  {columnShow.action === "action" && (
+                    <th scope="col">ACTION</th>
+                  )}
                 </tr>
               </thead>
               <tbody>
                 {users.map((user) => (
                   <tr key={user._id}>
-                    <td>
-                      <div className="user">
-                        <div>
-                          <img src={user.img} alt="user-img" />
+                    {columnShow.user === "user" && (
+                      <td>
+                        <div className="user">
+                          <div>
+                            <img src={user.img} alt="user-img" />
+                          </div>
+                          <div className="user-info">
+                            <p className="name">{user.name}</p>
+                            <p className="userName">{user.userName}</p>
+                          </div>
                         </div>
-                        <div className="user-info">
-                          <p className="name">{user.name}</p>
-                          <p className="userName">{user.userName}</p>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      <div className="email">{user.email}</div>
-                    </td>
-                    <td>
-                      {user.role === "Author" ? (
-                        <div className="role-alignment">
-                          <img src={author} alt="" />
-                          {user.role}
-                        </div>
-                      ) : user.role === "Maintainer" ? (
-                        <div className="role-alignment">
-                          <img src={maintainer} alt="" />
-                          {user.role}
-                        </div>
-                      ) : user.role === "Admin" ? (
-                        <div className="role-alignment">
-                          <img src={computer} alt="" />
-                          {user.role}
-                        </div>
-                      ) : user.role === "Editor" ? (
-                        <div className="role-alignment">
-                          <img src={vector} alt="" />
-                          {user.role}
-                        </div>
-                      ) : (
-                        <div className="role">
-                          <img src={subscriber} alt="" />
-                          {user.role}
-                        </div>
-                      )}
-                    </td>
-                    <td className='plan'>{user.plan}</td>
-                    <td>
-                      <div
-                        className={`${
-                          user.status === "Active"
-                            ? "active"
-                            : user.status === "Pending"
-                            ? "pending"
-                            : "inactive"
-                        }`}
-                      >
-                        {user.status}
-                      </div>
-                    </td>
-                    <td className="dropdown">
-                      <div>
-                        <button className="dropbtn">
-                          <BiDotsVertical />
-                        </button>
-                      </div>
-                      <div className="dropdown-content">
-                        <button className="btn-icon">
-                          <Link to={`/dashboard/updateUser/${user._id}`}>
-                            <CiEdit className="edit-icon" />
-                          </Link>
-                        </button>
-                        <button
-                          className="btn-icon"
-                        //   onClick={() => handleDelete(d._id)}
+                      </td>
+                    )}
+
+                    {columnShow.email === "email" && (
+                      <td>
+                        <div className="email">{user.email}</div>
+                      </td>
+                    )}
+
+                    {columnShow.role === "role" && (
+                      <td>
+                        {user.role === "Author" ? (
+                          <div className="role-alignment">
+                            <img src={author} alt="" />
+                            {user.role}
+                          </div>
+                        ) : user.role === "Maintainer" ? (
+                          <div className="role-alignment">
+                            <img src={maintainer} alt="" />
+                            {user.role}
+                          </div>
+                        ) : user.role === "Admin" ? (
+                          <div className="role-alignment">
+                            <img src={computer} alt="" />
+                            {user.role}
+                          </div>
+                        ) : user.role === "Editor" ? (
+                          <div className="role-alignment">
+                            <img src={vector} alt="" />
+                            {user.role}
+                          </div>
+                        ) : (
+                          <div className="role">
+                            <img src={subscriber} alt="" />
+                            {user.role}
+                          </div>
+                        )}
+                      </td>
+                    )}
+
+                    {columnShow.plan === "plan" && (
+                      <td className="plan">{user.plan}</td>
+                    )}
+
+                    {columnShow.status === "status" && (
+                      <td>
+                        <div
+                          className={`${
+                            user.status === "Active"
+                              ? "active"
+                              : user.status === "Pending"
+                              ? "pending"
+                              : "inactive"
+                          }`}
                         >
-                          <FaTrash />
-                        </button>
-                      </div>
-                    </td>
+                          {user.status}
+                        </div>
+                      </td>
+                    )}
+
+                    {columnShow.action === "action" && (
+                      <td className="dropdown">
+                        <div>
+                          <button className="dropbtn">
+                            <BiDotsVertical />
+                          </button>
+                        </div>
+                        <div className="dropdown-content">
+                          <button className="btn-icon">
+                            <Link to={`/dashboard/updateUser/${user._id}`}>
+                              <CiEdit className="edit-icon" />
+                            </Link>
+                          </button>
+                          <button
+                            className="btn-icon"
+                            //   onClick={() => handleDelete(d._id)}
+                          >
+                            <FaTrash />
+                          </button>
+                        </div>
+                      </td>
+                    )}
                   </tr>
                 ))}
               </tbody>
